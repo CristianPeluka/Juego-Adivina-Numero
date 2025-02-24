b04e16f30a2bc945b2e41de3b38f01396d0db7a7
Cristian Peluka <cristiancamposorue@gmail.com>
2025-02-23 22:42:21 -0500

    Se desarrollo la primera version del juego adivina el numero

--- Program.cs
+++ Program.cs
@@ -10,6 +10,39 @@ namespace AdivinaElNumero
     {
         static void Main(string[] args)
         {
+            int numeroIngresado;
+            Random numeroAle = new Random();
+            int aleatorio = numeroAle.Next(1, 51);
+            //Console.WriteLine(aleatorio);
+
+            do
+            {
+                Console.Write("Ingrese el numero a adivinar: ");
+                numeroIngresado = int.Parse(Console.ReadLine());
+
+                if (numeroIngresado == aleatorio)
+                {
+                    Console.WriteLine("Felicidades, Adivinaste el numero");
+                    break;
+                }
+                else
+                {
+                    if (numeroIngresado > aleatorio)
+                    {
+                        Console.WriteLine("El numero es menor");
+                    }
+                    if (numeroIngresado < aleatorio)
+                    {
+                        Console.WriteLine("El numero es mayor");
+                    }
+                }
+            }
+            while (true);
+            
+
+
+           
+
         }
     }
 }
